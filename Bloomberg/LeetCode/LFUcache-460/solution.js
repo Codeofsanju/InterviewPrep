ef541b96329e3c7196e8f5ea2c1427a4994c8829
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-04-11 11:29:40 -0400

    LFU cache - property setting properly, list ordering by freq correctly - TODO: invalidation of least used on cache capacity

--- Bloomberg/LeetCode/LFUcache-460/solution.js
+++ Bloomberg/LeetCode/LFUcache-460/solution.js
@@ -60,24 +60,40 @@ const LFUCache = function(cap) {
 };
 
 
+const sortHelper = function(list, cache){
+    list.sort((a, b) => {
+        return cache[a].freq > cache[b].freq ? 1 : -1;
+    });
+};
 
-
-LFUCache.prototype.get = (key) => {
+LFUCache.prototype.get = function(key) {
     if(!this.cache[key]){
         return -1;
     } else {
         this.cache[key].freq++;
-        this.list.sort((a, b) => {
-            return this.cache[a].freq > this.cache[b].freq ? 1 : -1;
-        });
+        // this.list.sort((a, b) => {
+        //     return this.cache[a].freq > this.cache[b].freq ? 1 : -1;
+        // });
+        sortHelper(this.list, this.cache);
         return this.cache[key];
     }
 };
 
-LFUCache.prototype.put = (key, value) => {
-    const node = new Node(value);
-    console.log(node);
-    this.cache[key] = node;
+LFUCache.prototype.put = function(key, value) {
+    // const node = new Node(value);
+    // console.log(node);
+    if(!this.cache[key]){
+        // check if cache.keys.length is equal to capacity here
+        // create helper function to take care of invalidation of least 
+        // frequently used
+
+        this.list.push(key);
+        this.cache[key] = new Node(value);
+    } else{
+        this.cache[key].freq++;
+        this.cache[key].val = value;
+        sortHelper(this.list, this.cache);
+    }
 };
 
 const LFU = new LFUCache(50);
@@ -85,3 +101,12 @@ console.log(LFU);
 LFU.put('key1', 1);
 LFU.put('key2', 2);
 LFU.put('key3', 3);
+console.log(LFU);
+LFU.get('key1');
+console.log(LFU);
+LFU.get('key2');
+console.log(LFU);
+LFU.put('key4', 4);
+LFU.put('key4', '4a');
+LFU.put('key4', '4b');
+console.log(LFU);
