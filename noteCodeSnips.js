9705e57afa2743b76ef542931c634ad281e4090a
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-04-16 12:42:18 -0400

    CSS- Advantages, frameworks, centering elements in CSS1

--- noteCodeSnips.js
+++ noteCodeSnips.js
@@ -208,3 +208,7 @@ var arr3 = "jones".split('');
 arr2.push(arr3);
 console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
 console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
+
+
+
+
