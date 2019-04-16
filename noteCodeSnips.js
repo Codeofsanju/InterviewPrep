e66e00cd825aa8c3750ead5a723a49cbfd8827e9
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-04-16 13:00:31 -0400

    JavaScript- Unary operators, order of operations, and automatic typecasting

--- noteCodeSnips.js
+++ noteCodeSnips.js
@@ -212,3 +212,9 @@ console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
 
 
 
+console.log(1 + "2" + "2"); // 122
+console.log(1 + +"2" + "2"); // 32
+console.log(1 + -"1" + "2"); // 02
+console.log(+"1" +  "1" + "2"); // 112
+console.log( "A" - "B" + "2"); // NaN2
+console.log( "A" - "B" + 2); // NaN
