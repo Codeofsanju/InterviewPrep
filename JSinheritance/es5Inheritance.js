e63d458815249fe67faae192c996b80778ab414d
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-05-22 19:35:06 -0400

    Prototype chain inhheritance (es5)

---
+++ JSinheritance/es5Inheritance.js
@@ -0,0 +1,61 @@
+// Base animal
+
+const Animal = function(name, energy){  
+    this.name = name;
+    this.energy = energy;
+};
+
+Animal.prototype.eat = function(){
+    this.energy += 1;
+    console.log(`Animal is eating, new energy: ${this.energy}`)
+};
+
+Animal.prototype.sleep = function(){
+    this.energy += 5;
+    console.log(`Animal is sleeping, new energy: ${this.energy}`);
+};
+
+Animal.prototype.play = function(){
+    this.energy -= 2;
+    console.log(`Animal is playing, new energy: ${this.energy}`);
+};
+
+
+const animal1 = new Animal('Bruno', 5);
+
+console.log(animal1);
+animal1.eat();
+animal1.play();
+animal1.sleep();
+
+
+
+// Doggo Class
+const Dog = function(name, energy, breed) {
+    Animal.call(this, name, energy); // setting this context to dog when calling Animal constructor
+    this.breed = breed;
+};
+
+Dog.prototype = Object.create(Animal.prototype); // Set Dogs prototype to Animals to inherit methods of animal
+Dog.prototype.constructor = Dog; // set constructor to Dog constructor, remember by 
+
+Dog.prototype.bark = function(){
+    console.log(`${this.name} goes BARK BARK`);
+};
+
+const Bruno = new Dog('Bruno', 10, 'Husky');
+
+console.log('\nBRUNO');
+Bruno.eat();
+Bruno.play();
+Bruno.sleep();
+Bruno.bark();
+
+// console.log(Object.getPrototypeOf(Bruno));
+
+console.log('\nPrototype chain of Bruno:');
+let curr = Bruno;
+while(curr){
+    console.log(curr);
+    curr = Object.getPrototypeOf(curr);
+};
