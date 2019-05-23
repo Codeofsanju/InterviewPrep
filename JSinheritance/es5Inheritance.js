e4b44b8529f4b9d9bbfb395ae705f19d9bbe45f7
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-05-22 20:38:26 -0400

    Prototype chain inhheritance (es5 edit, es6 classes approach )

--- JSinheritance/es5Inheritance.js
+++ JSinheritance/es5Inheritance.js
@@ -7,21 +7,21 @@ const Animal = function(name, energy){
 
 Animal.prototype.eat = function(){
     this.energy += 1;
-    console.log(`Animal is eating, new energy: ${this.energy}`)
+    console.log(`${this.name} is eating, new energy: ${this.energy}`)
 };
 
 Animal.prototype.sleep = function(){
     this.energy += 5;
-    console.log(`Animal is sleeping, new energy: ${this.energy}`);
+    console.log(`${this.name} is sleeping, new energy: ${this.energy}`);
 };
 
 Animal.prototype.play = function(){
     this.energy -= 2;
-    console.log(`Animal is playing, new energy: ${this.energy}`);
+    console.log(`${this.name} is playing, new energy: ${this.energy}`);
 };
 
 
-const animal1 = new Animal('Bruno', 5);
+const animal1 = new Animal('animal1', 5);
 
 console.log(animal1);
 animal1.eat();
@@ -30,7 +30,7 @@ animal1.sleep();
 
 
 
-// Doggo Class
+// Doggo Sub
 const Dog = function(name, energy, breed) {
     Animal.call(this, name, energy); // setting this context to dog when calling Animal constructor
     this.breed = breed;
@@ -59,3 +59,4 @@ while(curr){
     console.log(curr);
     curr = Object.getPrototypeOf(curr);
 };
+
---
+++ JSinheritance/es6Inheritance.js
@@ -0,0 +1,42 @@
+// Base Class Animal
+
+class Animal{
+    constructor(name, energy){
+        this.name = name;
+        this.energy = energy;
+    };
+    eat(){
+        this.energy += 1;
+        console.log(`Animal is eating, new energy: ${this.energy}`)
+    }
+    sleep(){
+        this.energy += 5;
+        console.log(`${this.name} is sleeping, new energy: ${this.energy}`);
+    }
+    play(){
+        this.energy -= 2;
+        console.log(`${this.name} is playing, new energy: ${this.energy}`);
+    }
+}
+
+class Dog extends Animal{
+    constructor(name, energy, breed){
+        super(name, energy);
+        this.breed = breed;
+    };
+    bark(){
+        console.log(`${this.name} goes BARK BARK`);
+    }
+}
+
+
+const Bruno = new Dog('Bruno', 10, 'Husky');
+
+
+console.log('Prototype Chain of Bruno using classes for inheritance');
+
+let curr = Bruno;
+while(curr){
+    console.log(curr);
+    curr = Object.getPrototypeOf(curr);
+}
