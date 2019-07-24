69949a890bfe167046c12af9d9b7ed80b296a26e
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-07-23 22:32:41 -0400

    3 ways to solve isPalimdrone

--- Algorithms in JS/AlgoCasts/exercises/palindrome/index.js
+++ Algorithms in JS/AlgoCasts/exercises/palindrome/index.js
@@ -7,6 +7,29 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+// ratchetng with 2 pointers O(logN)
+function palindrome(str) {
+    let left = 0;
+    let right = str.length-1;
+
+    while(left < right){
+        if(str[left] !== str[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+}
+
+// reversing and comparing O(n)
+function palindrome1(str){
+    const reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
+// using every
+function palindrome2(str){
+    return str.split('').every((char, index) => char === str[str.length - index-1]);
+}
 
 module.exports = palindrome;
