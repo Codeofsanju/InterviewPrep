78cb93694bd37e2a018f20b3d7a9bc79afd3421f
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-07-23 22:11:15 -0400

    4 different ways to reverse a string

--- Algorithms in JS/AlgoCasts/exercises/reversestring/index.js
+++ Algorithms in JS/AlgoCasts/exercises/reversestring/index.js
@@ -6,6 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
 
+// using array methods
+function reverse(str) {
+    return str.split('').reverse().join('');
+}
+
+
+// working with a right pointer
+function reverse1(str){
+    let ret = '';
+    let right = str.length-1;
+    while(right >= 0){
+        ret+=str[right];
+        right--;
+    }
+    return ret;
+}
+
+
+// using a for of
+function reverse2(str){
+    let reversed = '';
+    for(let character of str){
+        reversed = character + reversed; // we add each character behind the last
+    }
+    return reversed;
+}
+
+// using Reduce
+ function reverse3(str){
+     return str.split('').reduce((accum, char) => char + accum, '');
+ }
 module.exports = reverse;
