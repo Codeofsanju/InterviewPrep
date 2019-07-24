ce7b31140c5cf41a1138cf28a4b3957bf27144e5
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-07-24 10:29:02 -0400

    Solving maxChar / isAnagram/ hasRepeated using a map

--- Algorithms in JS/AlgoCasts/exercises/maxchar/index.js
+++ Algorithms in JS/AlgoCasts/exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+// using a charMap
+function maxChar(str) {
+    const charMap = {};
+
+    str.split('').map(char => {
+        if(!charMap[char]){
+            charMap[char] = 1;
+        }
+        else{
+            charMap[char]++;
+        }
+    });
+
+    let greatest = 0;
+    let greatestChar = '';
+    for(let key in charMap){
+        if(charMap[key] > greatest){
+            greatest = charMap[key];
+            greatestChar = key;
+        } 
+    }
+    return greatestChar;
+}
 
 module.exports = maxChar;
