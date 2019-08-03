e5433b24358c5c1fec81e0c8622ea36528fff787
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-08-03 19:14:15 -0400

    Queue: using array methods

--- Algorithms in JS/AlgoCasts/exercises/queue/index.js
+++ Algorithms in JS/AlgoCasts/exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor(){
+        this.list = [];
+    }
+
+    add(elem){
+        this.list.unshift(elem);
+    }
+
+    remove(){
+        return this.list.pop();
+    }
+}
 
 module.exports = Queue;
