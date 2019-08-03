5b2e881e7a2a8846cde9b10625d788a7fbc5559e
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-08-03 17:01:42 -0400

    Vowels: 1. Using charMap and 2. regex

--- Algorithms in JS/AlgoCasts/exercises/vowels/index.js
+++ Algorithms in JS/AlgoCasts/exercises/vowels/index.js
@@ -7,6 +7,23 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const charMap = new Set(['a', 'e', 'i', 'o', 'u']);
+    let count = 0;
+    str.split('').map(letter => {
+        charMap.has(letter.toLowerCase()) && count++;
+    });
+    return count;
+}
+
+// using regular expressions and .match -> return number of matches
+// g means to continue even after one is found and i means insensitive (case insensitive)
+function vowelsRegEx(str){
+    const matches = str.match(/['aeiou']/gi);
+
+    // if no matches are found, match return null so we must check it
+    return matches === null ? 0 : matches;
+}
+
 
 module.exports = vowels;
