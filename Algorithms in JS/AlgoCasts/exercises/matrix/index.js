911c3946e0db72e961b6c57218a193994d282e7d
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-08-03 18:30:36 -0400

    Spiral Matrix: Funky iteration

--- Algorithms in JS/AlgoCasts/exercises/matrix/index.js
+++ Algorithms in JS/AlgoCasts/exercises/matrix/index.js
@@ -15,6 +15,49 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const res = [];
+    for(let i = 0; i<n; i++){
+        res[i] = new Array(n-1).fill(0);
+    }
+
+    let rowStart = 0;
+    let rowEnd = n-1;
+    let columnStart = 0;
+    let columnEnd = n-1;
+    let counter = 1;
+
+    while(rowStart <= rowEnd && columnStart <= columnEnd){
+    //top row
+        for(let i = columnStart; i <= columnEnd; i++){
+            res[rowStart][i] = counter;
+            counter++;
+        }
+        rowStart++;
+        
+    //right column
+        for(let i = rowStart; i <= rowEnd; i++){
+            res[i][columnEnd] = counter;
+            counter++;
+        }
+        columnEnd--;
+
+    //bottom row
+        for(let i = columnEnd; i >= columnStart; i--){
+            res[rowEnd][i] = counter;
+            counter++;
+        }
+        rowEnd--;
+
+    //left column
+        for(let i = rowEnd; i >= rowStart; i--){
+            res[i][columnStart] = counter;
+            counter++;
+        }
+        columnStart++;
+    }
+    console.log(res);
+    return res;
+}
 
 module.exports = matrix;
