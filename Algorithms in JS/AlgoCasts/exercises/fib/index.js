d47eb7d89dd4dcf7f9298421749f0bdbce5165f3
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-08-04 14:03:41 -0400

    Fib Sequence- 3 ways: 2 ways iteratively + Recursively

--- Algorithms in JS/AlgoCasts/exercises/fib/index.js
+++ Algorithms in JS/AlgoCasts/exercises/fib/index.js
@@ -8,6 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
 
-module.exports = fib;
+
+// iterative solution using temp variable
+function fib(n) {
+    let curr = 1;
+    let prev = 0;
+    for(let i = 2; i <= n; i++){  // essentially starting at index 2 since we need the first two elements of sequence to start
+        let temp = curr;
+        curr = prev + curr;
+        prev = temp;
+    }
+    return curr;
+}
+
+// iteratuve using an array 
+function fib1(n){
+    let record = [0,1]; // fib sequence always starts with 0,1
+    for(let i = 2; i <= n ; i++){
+        record.push(record[i-1]+record[i-2]);
+    }
+    return record[n]; // or record[record.length-1]
+ }
+
+
+ // recursive appproach
+ function fibRec(n){
+    if(n===1 || n===0){
+        return n;
+    }
+
+    return fibRec(n-1) + fibRec(n-2);
+ }
+
+module.exports = fibRec;
