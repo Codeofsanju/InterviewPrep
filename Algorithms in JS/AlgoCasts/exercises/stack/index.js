a23e453b35276389d237476b77b17d00157205c3
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-08-04 15:45:31 -0400

    Stack: push, pop, peek

--- Algorithms in JS/AlgoCasts/exercises/stack/index.js
+++ Algorithms in JS/AlgoCasts/exercises/stack/index.js
@@ -10,6 +10,23 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+    constructor(){
+        this.data = [];
+    }
+
+    push(elem){
+        const newArr = [...this.data, elem];
+        this.data = newArr;
+    }
+
+    pop(){
+        return this.data.splice(this.data.length-1, 1)[0];
+    }
+
+    peek(){
+        return this.data[this.data.length-1];
+    }
+}
 
 module.exports = Stack;
