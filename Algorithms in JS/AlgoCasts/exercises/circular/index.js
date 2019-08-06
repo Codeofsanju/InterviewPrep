d09c58ea9caa0c403b224ec66c04e80067316e76
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-08-06 13:22:16 -0400

    Cycle detection of linkedList using slow and fast pointer approach + using a visited set

--- Algorithms in JS/AlgoCasts/exercises/circular/index.js
+++ Algorithms in JS/AlgoCasts/exercises/circular/index.js
@@ -12,6 +12,65 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+
+/*  Approach
+    - use the fast + slow variable approach
+        - fast: start at list.head 
+        - slow: start at list.head
+    - traverse list
+        - fast: advance 2 times each loop
+        - slow: advance 1 time each loop
+        Ex: 
+            node1 -> node2 -><- node3
+            fast
+            slow
+
+            node1 -> node2 -><- node3
+                     slow       fast
+
+            node1 -> node2 -><- node3
+                                fast
+                                slow
+
+    - return true if slow and fast are ever equal
+    - return false if fast reaches null and fast was never equal to slow
+
+*/
+
+
+function circular(list) {
+    let slow = list.head;
+    let fast = list.head;
+
+    while(fast.next && fast.next.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// or using a map to keep track of what has already been visited:
+
+function circularMap(list){
+    let visited = new Set();
+
+    let curr = list.head;
+
+    while(curr){
+        if(!visited.has(curr)){
+            visited.add(curr);
+        }
+        else{
+            return true;
+        }
+        curr = curr.next;
+    }
+
+    return false;
+}
 
 module.exports = circular;
