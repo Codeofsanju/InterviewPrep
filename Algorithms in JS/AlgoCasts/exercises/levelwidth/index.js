cf77e301052a6d24a14da6ca46260fe7e2c719e5
Sanjeev Sharma <sanjeev.sharma90@myhunter.cuny.edu>
2019-08-15 22:07:50 -0400

    Level Width - returning the width of each level in an array using bredth first traversal

--- Algorithms in JS/AlgoCasts/exercises/levelwidth/index.js
+++ Algorithms in JS/AlgoCasts/exercises/levelwidth/index.js
@@ -11,6 +11,23 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    if(!root) return [0];
+
+    const count = [0];
+    const queue = [root, 's'];
+
+    while(queue.length > 1){
+        const curr = queue.shift();
+        if(curr === 's'){
+            count.push(0);
+            queue.push(curr);
+            continue;
+        }
+        queue.push(...curr.children);
+        count[count.length-1]++;
+    }
+    return count;
+}
 
 module.exports = levelWidth;
